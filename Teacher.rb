45cef87c827f5bd5b3ea296d52c11674020e0421
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-11-08 21:15:16 +0200

    Create Teacher.rb

---
+++ Teacher.rb
@@ -0,0 +1,10 @@
+require './Person'
+class Teacher  < Person
+    def   initialize (specialization,age,parent_permission:true, name = "Unknown")
+    super(age,name,parent_permission)
+    @specialization=specialization
+    end
+    def can_use_services?
+        true
+    end
+end

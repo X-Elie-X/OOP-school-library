1e1f10ea5aa9ba33380e3354a9b3eb170acada8e
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-11-08 21:15:13 +0200

    Create Student.rb

---
+++ Student.rb
@@ -0,0 +1,10 @@
+require './Person'
+class Student < Person
+  def   initialize (classroom,age,parent_permission:true, name = "Unknown")
+    super(age,name,parent_permission)
+    @classroom = classroom
+  end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end

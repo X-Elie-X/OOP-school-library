8801627b3cd7e6dcc691be659a91c03ba63bfb05
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-11-11 10:37:02 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,17 +1,15 @@
 require './person'
 
 class Student < Person
-attr_reader :classroom
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-  end
-  def classroom
     @classroom = classroom
-    classroom.students.push(self)
-    unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-  '¯\(ツ)/¯'
+    '¯\(ツ)/¯'
   end
 end
